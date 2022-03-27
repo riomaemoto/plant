74d8b8513e5fc7cd5a36694d1bb9f7b183da7fa6
nagatomo <nagato1996502@icloud.com>
2022-03-27 23:22:07 +0900

    aaa変更

--- lib/widgets/TodoTile.dart
+++ lib/widgets/TodoTile.dart
@@ -1,56 +1,47 @@
 import 'package:flutter/material.dart';
 
 class TodoTile extends StatefulWidget {
+  const TodoTile({Key? key}) : super(key: key);
+
   @override
-  State<TodoTile> createState() => _TodoTileState();
+  _TodoTileState createState() => _TodoTileState();
 }
 
 class _TodoTileState extends State<TodoTile> {
-  // final bool isChecked;
   bool isChecked = false;
 
-  void checkboxCallback(bool checkboxstate) {
-    (newValue) {
-      setState(() {
-        isChecked = checkboxstate;
-      });
-    };
-    // final String title;
-    @override
-    Widget build(BuildContext context) {
-      return ListTile(
-        title: Text(
-          // title,
-          "this is a task",
-          style: TextStyle(
-              decoration: isChecked ? TextDecoration.lineThrough : null),
-        ),
-        trailing: TaskCheckbox(
-          checkboxState: isChecked,
-          toggleCheckboxState: (bool checkboxState) {
-            setState(() {
-              isChecked = checkboxstate;
-            });
-          },
-        ),
-      );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        "this is a task",
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
+      ),
+      trailing: Checkbox(
+        activeColor: Colors.lightBlueAccent,
+        value: isChecked,
+        onChanged: (newValue) {
+          isChecked = newValue!;
+          setState(() {});
+        },
+      ),
+    );
   }
 }
 
 class TaskCheckbox extends StatelessWidget {
-  final bool = checkboxState;
-  final Function toggleCheckBoxState;
-
-  TaskCheckbox({this.checkboxState, this.toggleCheckboxState});
+  final bool value;
+  TaskCheckbox({required this.value});
 
   @override
   Widget build(BuildContext context) {
     return Checkbox(
       activeColor: Colors.lightBlueAccent,
-      // value: isChecked,
-      value: checkboxState,
-      onChanged: toggleCheckboxState,
+      value: false,
+      onChanged: (newValue) {
+        print(newValue);
+      },
     );
   }
 }
