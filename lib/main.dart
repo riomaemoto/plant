380105e933bfdde4fb742e22cb1ec9870e8fe831
nagatomo <nagato1996502@icloud.com>
2022-03-30 00:45:15 +0900

    BigAss project start

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(BigAss());
@@ -24,7 +26,33 @@ class BallPage extends StatelessWidget {
         title: Text("Ask me anything"),
         backgroundColor: Colors.blue[900],
       ),
-      body: Container(),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({Key? key}) : super(key: key);
+
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  Random ballNumber = new Random();
+  int number = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        child: Image.asset('images/ball$number.png'),
+        onPressed: () {
+          number = ballNumber.nextInt(5) + 1;
+          setState(() {});
+          // print(ballNumber.nextInt(5));
+        },
+      ),
     );
   }
 }
