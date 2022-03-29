80e711a8c2604d989d236e49ea7280a5afb1858f
nagatomo <nagato1996502@icloud.com>
2022-03-30 00:54:36 +0900

    destini start

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:plant/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(BigAss());
+    await tester.pumpWidget(Destini());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
