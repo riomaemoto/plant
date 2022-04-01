d51d18b610aa263c53873ef269f0be7bcff567eb
nagatomo <nagato1996502@icloud.com>
2022-04-01 19:02:22 +0900

    bitcoin start

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:plant/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(Destini());
+    await tester.pumpWidget(Bitcoin());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
