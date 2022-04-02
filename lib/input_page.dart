a5f2b1ceb05eb38ceb1d5391da8775f83ad6b1f6
nagatomo <nagato1996502@icloud.com>
2022-04-03 01:23:17 +0900

    BMI calculator

---
+++ lib/input_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatelessWidget {
+  const InputPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BMI Calculator"),
+      ),
+      body: SizedBox(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:plant/price_screen.dart';
 
-void main() => runApp(Bitcoin());
+import 'input_page.dart';
 
-class Bitcoin extends StatelessWidget {
-  const Bitcoin({Key? key}) : super(key: key);
+void main() => runApp(BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  const BMICalculator({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: PriceScreen(),
-      theme: ThemeData.dark().copyWith(
-          primaryColor: Colors.lightBlue,
-          scaffoldBackgroundColor: Colors.white),
+      home: InputPage(),
     );
   }
 }
